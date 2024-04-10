f6739ad9fda4f4f1b27bfebc55a9892bc22dfb91
Tiare Infante <tiare.infante.snmartin@gmail.com>
2024-04-10 17:41:49 -0400

    prueba 3

--- api/index.js
+++ api/index.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const config = require('../src/config');
+const authMiddleware = require('../src/middleware/auth');
+const errorHandler = require('../src/middleware/error');
+const routes = require('../src/routes');
+const pkg = require('../package.json');
 
+const { port, secret } = config;
 const app = express();
 
-app.get('/', (req, res) => res.send('Express on Vercel'));
+app.set('config', config);
+app.set('pkg', pkg);
 
-app.listen(8080, () => console.log('Server ready on port 3000.'));
+// parse application/x-www-form-urlencoded
+// Configurar middleware para analizar cuerpos de solicitudes con formato x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+// Configurar middleware para analizar cuerpos de solicitudes con formato JSON
+app.use(express.json());
+// Configurar middleware de autenticación con una clave secreta
+app.use(authMiddleware(secret));
 
-module.exports = app;
+// Registrar rutas
+routes(app, (err) => {
+  if (err) {
+    console.error('Error al registrar rutas:', err);
+    throw err;
+  }
+
+  app.use(errorHandler);
+
+  app.listen(port, () => {
+    console.info(`App listening on port ${port}`);
+  });
+});
